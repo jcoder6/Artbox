2092f6f182255f149ee9200121f6f70fc4e386f6
Jomer Dorego <jdorego06@gmail.com>
2022-03-23 13:14:19 +0800

    Make the feature Responsive and Fix the error when clicked links in menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,7 +1,19 @@
 const burger = document.querySelector(".burger");
 const menuModal = document.querySelector(".header__menu");
+const body = document.querySelector('body');
 
 burger.addEventListener('click', function() {
   burger.classList.toggle("burger-open");
   menuModal.classList.toggle("menu-open");
+  body.classList.toggle("scroll-none");
+});
+
+const menuLinks = document.querySelectorAll('.menu-links');
+
+menuLinks.forEach((menuLink) => {                        // FUNCTION THAT REMOVE ALL THE CLASS LIST WE ADDED TO HTML DOCUMENT
+  menuLink.addEventListener('click', function() {
+    burger.classList.remove("burger-open");
+    menuModal.classList.remove("menu-open");
+    body.classList.remove("scroll-none");
+  })
 })

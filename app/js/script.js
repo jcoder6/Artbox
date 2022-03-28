d13b35ca768d05076b05ca8ebe9c4a6b1a11ee06
Jomer Dorego <jdorego06@gmail.com>
2022-03-28 16:43:14 +0800

    generate the json testimonials

--- app/js/script.js
+++ app/js/script.js
@@ -17,3 +17,47 @@ menuLinks.forEach((menuLink) => {                        // FUNCTION THAT REMOVE
     body.classList.remove("scroll-none");
   })
 })
+
+// Get the container where we should put our testimonials item
+const testiContainer = document.querySelector('.testi-item-cont');
+
+//Get the file form json
+const testimonials = new XMLHttpRequest();
+testimonials.open("GET", "testimonials.json", true);
+testimonials.send();
+
+// check if we succesfully get the file and if it in readystate
+testimonials.onreadystatechange = function() {
+  if(this.readyState === 4){
+
+    //fetch the item we get and convert it into array
+    const testiItems = JSON.parse(testimonials.responseText);
+    getEachTesti(testiItems);
+  }
+}
+
+function getEachTesti(items){
+  // console.log(items);
+  const eachItems = items.testi;
+  // console.log(eachItems[0].name);
+  // Compile each testimonials to whole html format
+  const htmlTesti = compileItems(eachItems);
+  // console.log(htmlTesti);
+  testiContainer.innerHTML = htmlTesti;
+}
+
+function compileItems(eachItems){
+  let collectItem = eachItems.map((eachItem) => {
+
+    // we collected all the data in the json then put it on html format so that we can put our page
+    return `<article class="testi-item">
+        <div class="item-img">
+        <img src="${eachItem.img}" alt="${eachItem.name}" />
+        </div>
+        <h3>${eachItem.name}</h3>
+        <p>${eachItem.desc}</p>
+      </article>`
+  }).join("");
+
+  return collectItem;
+}
